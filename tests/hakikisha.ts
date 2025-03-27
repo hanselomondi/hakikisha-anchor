29f14be326b7ff34042de0d4cb9bc06c36a3d5fb
Hansel Omondi <omondihansel@students.uonbi.ac.ke>
2025-03-27 23:57:49 +0300

    Add: register_manufacturer, register_retailer test
    Unit tests for the two instruction handlers

--- tests/hakikisha.ts
+++ tests/hakikisha.ts
@@ -23,4 +23,57 @@ describe("hakikisha", () => {
 
     await fundWallet(manufacturerKp.publicKey, 2_000_000_000);
   });
+
+  // 1. Admin Flow: Register Manufacturer
+  it("can register a manufacturer", async () => {
+    const [manufacturerAccountPda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("manufacturer"), manufacturerKp.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+    .registerManufacturer("ABC Distillers", "KEBS123")
+    .accounts({
+      admin: program.provider.publicKey,
+      manufacturer: manufacturerAccountPda,
+      manufacturerWallet: manufacturerKp.publicKey,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([program.provider.wallet.payer])
+    .rpc();
+
+    const manufacturerAccount = await program.account.manufacturer.fetch(manufacturerAccountPda);
+    console.log(manufacturerAccount);
+    expect(manufacturerAccount.name).to.equal("ABC Distillers");
+    expect(manufacturerAccount.walletAddress.toBase58()).to.equal(manufacturerKp.publicKey.toBase58());
+    expect(manufacturerAccount.licenseNumber).to.equal("KEBS123");
+    expect(manufacturerAccount.isVerified).to.equal(true);
+  });
+
+  // 2. Admin Flow: Register Retailer
+  it("can register a retailer", async () => {
+    const [retailerAccountPda] = PublicKey.findProgramAddressSync(
+      [Buffer.from("retailer"), retailerKp.publicKey.toBuffer()],
+      program.programId
+    );
+
+    await program.methods
+    .registerRetailer("XYZ Retailers", "KEBS456")
+    .accounts({
+      admin: program.provider.publicKey,
+      retailer: retailerAccountPda,
+      retailerWallet: retailerKp.publicKey,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([program.provider.wallet.payer])
+    .rpc();
+
+    const retailerAccount = await program.account.retailer.fetch(retailerAccountPda);
+    console.log(retailerAccount);
+    expect(retailerAccount.name).to.equal("XYZ Retailers");
+    expect(retailerAccount.walletAddress.toBase58()).to.equal(retailerKp.publicKey.toBase58());
+    expect(retailerAccount.licenseNumber).to.equal("KEBS456");
+    expect(retailerAccount.isVerified).to.equal(true);
+  });
+
 });
