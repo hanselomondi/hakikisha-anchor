e63eb631a31001c0892f08978cfb5ca0d8eeb529
Hansel Omondi <omondihansel@students.uonbi.ac.ke>
2025-03-27 23:42:30 +0300

    Add: beforeEach function in hakikisha.ts
    This function initialises global variables to be used across all unit
    tests

--- tests/hakikisha.ts
+++ tests/hakikisha.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Hakikisha } from "../target/types/hakikisha";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("hakikisha", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +10,17 @@ describe("hakikisha", () => {
 
   const program = anchor.workspace.Hakikisha as Program<Hakikisha>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // Variables to be used in the tests
+  let manufacturerKp = Keypair.generate();
+  let retailerKp = Keypair.generate();
+  let consumerKp = Keypair.generate();
+
+  beforeEach(async () => {
+    const fundWallet = async (publicKey: PublicKey, amount: number) => {
+      const tx = await program.provider.connection.requestAirdrop(publicKey, amount);
+      await program.provider.connection.confirmTransaction(tx);
+    };
+
+    await fundWallet(manufacturerKp.publicKey, 2_000_000_000);
   });
 });
