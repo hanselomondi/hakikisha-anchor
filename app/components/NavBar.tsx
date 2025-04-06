1483c1f6f16333f0aa03c6fdbbee822395625048
Hansel Omondi <omondihansel@students.uonbi.ac.ke>
2025-04-06 21:18:49 +0300

    feat: sign-up and sign-in pages
    These two pages will be used for user registration and on-boarding on
    the platform

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { HandMetal, House } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
+      <div className='container flex items-center justify-between px-3 py-1'>
+        <Link href='/'>
+          <House />
+        </Link>
+        <Link className={buttonVariants()} href='/sign-in'>
+          Sign in
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
